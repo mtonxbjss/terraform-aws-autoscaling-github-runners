aca5cfd4eebc90651e49ec776fb716b678f715df
Mark Tonks <mark.tonks@bjss.com>
2023-07-07 09:52:20 +0100

    Dynamically set parameters in the dashboard

--- cloudwatch_dashboard_account.tf
+++ cloudwatch_dashboard_account.tf
@@ -5,12 +5,16 @@ resource "aws_cloudwatch_dashboard" "account" {
   dashboard_body = templatefile(
     "${path.module}/templates/github_dashboard_template.tmpl.json",
     {
-      RUNNER_NAME            = var.ec2_github_runner_name
-      UPPER_RUNNER_NAME      = upper(var.ec2_github_runner_name)
-      AUTOSCALING_GROUP_NAME = aws_autoscaling_group.github_runner.name
-      METRICS_NAMESPACE      = local.cloudwatch_logs_metric_filters_namespace
-      INSTANCE_TYPE          = var.ec2_instance_type
-      TAG_LIST               = var.ec2_github_runner_tag_list
+      RUNNER_NAME               = var.ec2_github_runner_name
+      UPPER_RUNNER_NAME         = upper(var.ec2_github_runner_name)
+      AUTOSCALING_GROUP_NAME    = aws_autoscaling_group.github_runner.name
+      METRICS_NAMESPACE         = local.cloudwatch_logs_metric_filters_namespace
+      INSTANCE_TYPE             = var.ec2_instance_type
+      TAG_LIST                  = var.ec2_github_runner_tag_list
+      CONCURRENT_JOBS           = var.ec2_maximum_concurrent_github_jobs
+      SCALE_IN_AT_FREE_CAPACITY = var.ec2_maximum_concurrent_github_jobs * 3
+      Y_AXIS_MIN                = var.ec2_maximum_concurrent_github_jobs * -2
+      Y_AXIS_MAX                = var.ec2_maximum_concurrent_github_jobs * 4
     }
   )
 }
